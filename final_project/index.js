2e20e723166c9a15d353d1f1dc5990d2713c543e
martoon01 <martoonsherpa01@gmail.com>
2025-03-04 04:09:34 -0500

    authentication

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+
+        // JWT verify for user authentication
+        jwt.verify(token, "access", (err, customer) => {
+            if (!err) {
+                req.customer = customer;
+                next(); // proceed to next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+
+        // return error if token verification fails
+    } else {
+        return res.status(403).json({ message : "User not logged in"});
+    }
 });
  
 const PORT =5000;
